83d9e583749cbcd0992154d3fc780af7f4c80499
Timofei Kirov <2005tima08@gmail.com>
2025-01-31 21:03:37 +0700

    Расслабился за добавлением объектов класса...

--- scripts/SpawnE.cs
+++ scripts/SpawnE.cs
@@ -9,11 +9,15 @@ public static class EntitySpawner
 
         var wolf = new Entity("Волк", 50, 10, 5, new List<Effect> { burningEffect });
         var outlaw = new Entity("Разбойник", 100, 20, 10, new List<Effect> { poisonEffect });
-        var vivern = new Entity("Вивера", 300, 50, 25);
+        var vivern = new Entity("Виверна", 300, 50, 25);
+		var shapeshifter = new Entity("Оборотень", 300, 30, 0, new List<Effect> { poisonEffect });
+		var warrior_of_nfrcturia = new Entity("Воин Северной Фрактурии", 100, 20, 30);
 
-        EntityList.Add(goblin);
-        EntityList.Add(orc);
-        EntityList.Add(dragon);
+        EntityList.Add(wolf);
+        EntityList.Add(outlaw);
+        EntityList.Add(vivern);
+		EntityList.Add(shapeshifter);
+		EntityList.Add(warrior_of_nfrcturia)
     }
 
     public static Entity SpawnRandomEntity()
