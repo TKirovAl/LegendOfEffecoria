8c6e6d6a9883e09413c781ee8402f72093c3fd95
Timofei Kirov <2005tima08@gmail.com>
2025-02-01 20:48:43 +0700

    Пополнил список сущностей новыми объектами...

--- scripts/SpawnE.cs
+++ scripts/SpawnE.cs
@@ -7,17 +7,26 @@ public static class EntitySpawner
         var burningEffect = new Effect("Горение", "Наносит 5 урона каждый ход.", 3);
         var poisonEffect = new Effect("Яд", "Наносит 3 урона каждый ход.", 4);
 
-        var wolf = new Entity("Волк", 50, 10, 5, new List<Effect> { burningEffect });
-        var outlaw = new Entity("Разбойник", 100, 20, 10, new List<Effect> { poisonEffect });
+        var wolf = new Entity("Волк", 50, 10, 0, new List<Effect> { burningEffect });
         var vivern = new Entity("Виверна", 300, 50, 25);
+        var bargest = new Entity("Баргест", 150, 70, 0);
+        var gidra = new Entity("Гидра", 100000, 50, 0);
+
+        var outlaw = new Entity("Разбойник", 100, 20, 10, new List<Effect> { poisonEffect });
+        var warrior_of_nfrcturia = new Entity("Воин Северной Фрактурии", 100, 20, 30);
+       
 		var shapeshifter = new Entity("Оборотень", 300, 30, 0, new List<Effect> { poisonEffect });
-		var warrior_of_nfrcturia = new Entity("Воин Северной Фрактурии", 100, 20, 30);
+		var aniuka = new Entity("Аниука", 30, 10, 0);
+    
 
         EntityList.Add(wolf);
         EntityList.Add(outlaw);
         EntityList.Add(vivern);
 		EntityList.Add(shapeshifter);
-		EntityList.Add(warrior_of_nfrcturia)
+		EntityList.Add(warrior_of_nfrcturia);
+        EntityList.Add(aniuka);
+        EntityList.Add(bargest);
+        EntityList.Add(gidra);
     }
 
     public static Entity SpawnRandomEntity()
